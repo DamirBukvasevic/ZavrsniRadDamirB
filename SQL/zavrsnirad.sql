2e2b3ec7c7a18d0be9097a22143ec85118d8cac3
DamirBukvasevic <damirbukvasevic@gmail.com>
2024-05-23 22:01:33 +0200

    Promjena naziva u tablicama, insert naredbe dobavljaci,artikl

--- SQL/zavrsnirad.sql
+++ SQL/zavrsnirad.sql
@@ -1,51 +1,68 @@
 ﻿use master;
 go
-drop database if exists zavrsnirad;
+drop database if exists ZavrsniRad;
 go
-create database zavrsnirad;
+create database ZavrsniRad;
 go
 
-use zavrsnirad;
+use ZavrsniRad;
 
-create table dobavljaci(
-sifra int not null primary key identity(1,1),
-naziv varchar(50) not null,
-grad varchar(20) not null,
-mjesto varchar(50) not null,
-oib char(11) not null
+create table Dobavljaci(
+Sifra int not null primary key identity(1,1),
+Naziv varchar(50) not null,
+Grad varchar(20) not null,
+Adresa varchar(50) not null,
+Oib char(11) not null
 );
 
 
-create table nabava(
-sifra int not null primary key identity(1,1),
-brojnabave int not null,
-datumnabave datetime not null,
-dobavljac int not null
+create table Nabava(
+Sifra int not null primary key identity(1,1),
+Broj_nabave int not null,
+Datum_nabave datetime not null,
+Dobavljac int not null
 );
 
 
-create table stavka(
-sifranabave int not null,
-sifraartikla int not null,
-kolicinaartikla int not null,
-cijena decimal(18,2) not null
+create table Stavka(
+Sifra_nabave int not null,
+Sifra_artikla int not null,
+Kolicina_artikla int not null,
+Cijena decimal(18,2) not null
 );
 
 
-create table artikl(
-sifra int not null primary key identity(1,1),
-nazivartikla varchar(50) not null,
-cijena decimal(18,2) not null
+create table Artikl(
+Sifra int not null primary key identity(1,1),
+Naziv_artikla varchar(50) not null,
+Cijena decimal(18,2) not null
 );
 
 
-alter table nabava add foreign key (dobavljac) references dobavljaci(sifra);
-alter table stavka add foreign key (sifranabave) references nabava(sifra);
-alter table stavka add foreign key (sifraartikla) references artikl(sifra);
+alter table Nabava add foreign key (Dobavljac) references Dobavljaci(Sifra);
+alter table Stavka add foreign key (Sifra_nabave) references Nabava(Sifra);
+alter table Stavka add foreign key (Sifra_artikla) references Artikl(Sifra);
 
 -- INSERT NAREDBE
+
 --select * from dobavljaci;
-insert into dobavljaci (naziv, grad, mjesto, oib)values
+insert into Dobavljaci (Naziv, Grad, Adresa, Oib)values
 ('Bijelic.doo','Osijek','Sv.Roka1',96752332364),
 ('Atlantic.doo','Osijek','M.Gupca1',48037484195),
 ('Roto.doo','Osijek','Gunduliceva1',82770989192);
+
+--select * from artikl;
+insert into Artikl (Naziv_artikla, Cijena)values
+('Coca-Cola 0.25 l', 01.27),
+('Coca-ColaZero 0.25 l', 01.27),
+('Fanta 0.25 l', 01.19),
+('Sprite 0.25 l', 01.18),
+('Schweppes 0.25 l', 01.15),
+('Pan 0.5 l', 01.37),
+('Tuborg 0.5 l', 01.35),
+('Kozel 0.5 l', 01.36),
+('Budweiser 0.5 l', 01.39),
+('Osjecko 0.5 l', 01.50),
+('Cedevita naranca 19 g', 00.85),
+('Cedevita limun 19 g', 00.85),
+('Cedevita grejp 19 g', 00.85);
